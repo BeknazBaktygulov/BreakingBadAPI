2ff18f192c0477710e215db4cac9c2a6097cb400
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-15 12:15:18 -0700

    styling

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../logo.png';
+
+function Header() {
+    return (
+        <header className="center">
+            <img src={logo} alt="breakingbad" />
+        </header>
+    )
+}
+
+export default Header
