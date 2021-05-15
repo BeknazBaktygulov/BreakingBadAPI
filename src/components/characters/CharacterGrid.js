05f0342e55604428ee409c2d673d4e647673509e
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-15 13:37:41 -0700

    Developed spinner widget when loading data

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CharacterItem from './CharacterItem';
 
+
 function CharacterGrid({ isLoading, items }) {
     return isLoading ? (<h1>Loading...</h1>) : <section className="cards">
         {items.map(item => (
---
+++ src/components/ui/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Spinner from '../../spinner.gif'
+
+function Spinner() {
+    return (
+                <img src={Spinner}  style={{ width: '200px', margin: 'auto', display: 'block'}}   alt="spinning circle" />
+    )
+}
+
+export default Spinner
