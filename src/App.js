5582ba0cc057b64820210a22836d82992180362e
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-15 12:28:33 -0700

    Brought in axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      console.log(result.data)
+      setItems(result.data)
+    }
+    fetchItems();
+  }, [])
+
   return (
     <div className="container">
-     <h1>Hello there</h1>
+     <p>{items}</p>
      <Header />
     </div>
   );
