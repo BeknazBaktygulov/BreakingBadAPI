e6164429f1f7f85d8216b73dc0a0f31fbb5979ed
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-15 12:49:22 -0700

    Displaying fetched data in character grid

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 function CharacterGrid({ isLoading, items }) {
     return isLoading ? (<h1>Loading...</h1>) : <section className="cards">
         {items.map(item => (
-            <h1>{item.name}</h1>
+            <h1 key={item.char_id}>{item.name}</h1>
         ))}
     </section>
 }
